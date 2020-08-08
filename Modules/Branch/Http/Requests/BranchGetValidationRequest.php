0789dc6f6a5e6e9d737348f9acd2fff37a520461
aristasoluciones <isc061990@outlook.com>
2020-08-08 15:44:07 -0500

    filtro para sucursales

--- Modules/Branch/Http/Requests/BranchGetValidationRequest.php
+++ Modules/Branch/Http/Requests/BranchGetValidationRequest.php
@@ -4,6 +4,8 @@ namespace Modules\Branch\Http\Requests;
 
 use App\Http\Requests\Request;
 use Config;
+use Modules\User\Models\User;
+
 /**
  * Class BranchGetValidationRequest
  * @package Modules\Branch\Http\Requests
@@ -24,5 +26,21 @@ class BranchGetValidationRequest extends Request {
      */
     public function rules() {
         return Config::get('validation.default_http_get');
+    }
+    /*
+     * @return void
+     */
+    protected function prepareForValidation() :void {
+        if($this->user()->role !== User::ROLE_ADMIN) {
+            $companyPredicates = [
+                'name' => 'branch_company_id',
+                'comparison' => 'equals',
+                'attribute' => 'branch.company_id',
+                'value' => $this->user()->company_id,
+            ];
+            $currentPredicates = $this->input()['predicates'] ?? [];
+            array_push($currentPredicates, $companyPredicates);
+            $this->merge(['predicates'=> $currentPredicates]);
+        }
     }
 }
