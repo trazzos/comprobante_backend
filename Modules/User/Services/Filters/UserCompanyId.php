5b40d5c5eaf407cc478e3d84ddd98484fe598d14
aristasoluciones <isc061990@outlook.com>
2020-08-08 15:52:11 -0500

    avances user

---
+++ Modules/User/Services/Filters/UserCompanyId.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+namespace Modules\User\Services\Filters;
+
+use App\Abstracts\FilterAbstract;
+use App\Repositories\Interfaces\CriteriaInterface;
+use App\Repositories\Interfaces\RepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CompanyId
+ * @package Modules\Company\Services\Filters
+ */
+class UserCompanyId extends FilterAbstract implements CriteriaInterface {
+    /**
+     * @var array $predicate
+     */
+    protected  array $predicate;
+
+    /**
+     * @param array $predicate
+     */
+    public function  __construct(array $predicate) {
+        $this->predicate = $predicate;
+    }
+
+    /**
+     * @param Model|Builder $model
+     * @param RepositoryInterface $repository
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository) : Builder {
+        return $this->{$this->predicate['comparison']}($model, $this->predicate);
+    }
+}
